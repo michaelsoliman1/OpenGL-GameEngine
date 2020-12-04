0528e42de83804347c88e67321382b92ca1150df
Michael <michaelsoliman981@gmail.com>
2020-12-04 15:57:48 +0200

    restruturing and cleaning(removed imgui)

--- source/core/application.hpp
+++ source/core/application.hpp
@@ -6,7 +6,6 @@
 #include <GLFW/glfw3.h>
 #include <imgui.h>
 
-#include <game_states/test_state.hpp>
 #include "../input/keyboard.hpp"
 #include "../input/mouse.hpp"
 
@@ -25,40 +24,15 @@ namespace our {
     class Application {
     protected:
         GLFWwindow * window = nullptr;      // Pointer to the window created by GLFW using "glfwCreateWindow()".
-        Keyboard keyboard;                  // Instance of "our" keyboard class that handles keyboard functionalities.
-        Mouse mouse;                        // Instance of "our" mouse class that handles mouse functionalities.
-
-        TestState *testState = new TestState();
-
-        // Virtual functions to be overrode and change the default behaviour of the application
-        // according to the example needs.
-        virtual void configureOpenGL();                             // This function sets OpenGL Window Hints in GLFW.
-        virtual WindowConfiguration getWindowConfiguration();       // Returns the WindowConfiguration current struct instance.
-        virtual void setupCallbacks();                              // Sets-up the window callback functions from GLFW to our (Mouse/Keyboard) classes.
+        void configureOpenGL();                             // This function sets OpenGL Window Hints in GLFW.
+        WindowConfiguration getWindowConfiguration();       // Returns the WindowConfiguration current struct instance.
 
     public:
-//        virtual void onInitialize(){}                   // Called once before the game loop.
-        virtual void onImmediateGui(ImGuiIO& io){}      // Called every frame to draw the Immediate GUI (if any).
-//        virtual void onDraw(double deltaTime){}         // Called every frame in the game loop passing the time taken to draw the frame "Delta time".
-//        virtual void onDestroy(){}                      // Called once after the game loop ends for house cleaning.
-
-
-        // Override these functions to get mouse and keyboard event.
-        virtual void onKeyEvent(int key, int scancode, int action, int mods){}
-        virtual void onCursorMoveEvent(double x, double y){}
-        virtual void onCursorEnterEvent(int entered){}
-        virtual void onMouseButtonEvent(int button, int action, int mods){}
-        virtual void onScrollEvent(double x_offset, double y_offset){}
-
-        int run();      // This is the main class function that run the whole application (Initialize, Game loop, House cleaning).
+        GLFWwindow* init();
 
         // Class Getters.
         GLFWwindow* getWindow(){ return window; }
         [[nodiscard]] const GLFWwindow* getWindow() const { return window; }
-        Keyboard& getKeyboard() { return keyboard; }
-        [[nodiscard]] const Keyboard& getKeyboard() const { return keyboard; }
-        Mouse& getMouse() { return mouse; }
-        [[nodiscard]] const Mouse& getMouse() const { return mouse; }
 
         // Get the size of the frame buffer of the window in pixels.
         glm::ivec2 getFrameBufferSize() {
@@ -77,6 +51,5 @@ namespace our {
     };
 
 }
-
 
 #endif //APPLICATION_H
--- source/core/game.cpp
+++ source/core/game.cpp
@@ -1,5 +1,34 @@
 #include "application.hpp"
+#include <game_states/test_state.hpp>
 
-int main(int argc, char** argv) {
-    return our::Application().run();
+int main() {
+    our::Application config;
+    GLFWwindow* window = config.init();
+    TestState* testState = new TestState();
+
+    testState->onEnter();
+
+    double last_frame_time = glfwGetTime();
+    while(!glfwWindowShouldClose(window)){
+        glfwPollEvents(); // Read all the user events and call relevant callbacks.
+
+        // Get the current time (the time at which we are starting the current frame).
+        double current_frame_time = glfwGetTime();
+
+        testState->onDraw(current_frame_time - last_frame_time);
+
+        // Then update the last frame start time (this frame is now the last frame)
+        last_frame_time = current_frame_time;
+
+        // Swap the frame buffers
+        glfwSwapBuffers(window);
+    }
+
+    testState->onExit();
+
+    // Destroy the window
+    glfwDestroyWindow(window);
+
+    // And finally terminate GLFW
+    glfwTerminate();
 }
