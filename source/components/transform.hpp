5b98e829c1f94dc3038a48dec6f2c5a6666139ec
Michael <michaelsoliman981@gmail.com>
2021-01-16 23:03:59 +0200

    added transfrom children

--- source/components/transform.hpp
+++ source/components/transform.hpp
@@ -6,6 +6,9 @@
 class Transform : public IComponent {
 private:
     glm::vec3 translation,  rotation, scale;
+    std::vector<Transform*> children = {};
+    glm::mat4 parentMatrix = glm::mat4(1.0f);
+    bool _isFree = false;
 
 public:
     Transform(
@@ -15,8 +18,26 @@ public:
     ): translation(translation), rotation(rotation), scale(scale) {}
 
     glm::mat4 to_mat4() const {
-        return glm::translate(glm::mat4(1.0f), translation) *
+        return parentMatrix * glm::translate(glm::mat4(1.0f), translation) *
                glm::yawPitchRoll(rotation.y, rotation.x, rotation.z) *
                glm::scale(glm::mat4(1.0f), scale);
     }
+    void addChild(Transform* child){
+        children.push_back(child);
+    }
+    void setParentTransform(glm::mat4 _parentMatrix){
+        parentMatrix = _parentMatrix;
+    }
+
+    std::vector<Transform*> getChildren(){
+        return children;
+    }
+
+    bool isParent(){return !children.empty();}
+    bool isFree(){return _isFree;}
+    void freeMe(){
+        parentMatrix = glm::mat4(1.0f);
+        _isFree = true;
+    }
+
 };
